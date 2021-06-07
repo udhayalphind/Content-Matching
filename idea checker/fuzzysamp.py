99f32f80c2deaa9b4e43818264474ebef139a4d5
udhayalphind <pandian.deva@gmail.com>
2021-06-07 11:44:28 +0530

    Add files via upload

---
+++ idea checker/fuzzysamp.py
@@ -0,0 +1,21 @@
+
+# from fuzzywuzzy import fuzz 
+# from fuzzywuzzy import process
+# a= fuzz.ratio('ideas', 'idea') 
+# b= fuzz.partial_ratio('idea working New York Jets New York Giants Dallas Cowboys', 'working')
+# c=fuzz.token_sort_ratio('ideas working New York Jets New York Giants Dallas Cowboys', 'Idea working')
+# d = fuzz.UWRatio('ideas working New York Jets New York Giants Dallas Cowboys', 'Idea working')
+# print(a)
+# print(d)
+# print(c)
+
+# from fuzzywuzzy import process
+# str2Match = "ideas working New York Jets New York Giants Dallas Cowboys"
+# strOptions = ["Idea work", "rama bala", "Praveen new"]
+# Ratios = process.extract(str2Match,strOptions)
+# print(Ratios)
+# # You can also select the string with the highest matching percentage
+# highest = process.extractOne(str2Match,strOptions)
+# print(highest)
+
+   
